ca024217f74f2f73238ee67b6cd4eeb0c4bba89b
Yassine Said Ali Ahmed <ysaa@blocklinkmaker.com>
2025-07-21 12:58:46 +0200

    change private key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,6 @@ dotenv.config();
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
-const HOLESKY_RPC_URL = process.env.HOLESKY_RPC_URL || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
@@ -19,17 +18,11 @@ const config: HardhatUserConfig = {
       url: SEPOLIA_RPC_URL,
       accounts: [`0x${PRIVATE_KEY}`],
       chainId: 11155111
-    },
-    holesky: {
-      url: HOLESKY_RPC_URL,
-      accounts: [`0x${PRIVATE_KEY}`],
-      chainId: 17000
     }
   },
   etherscan: {
     apiKey: {
-      sepolia: ETHERSCAN_API_KEY,
-      holesky: ETHERSCAN_API_KEY
+      sepolia: ETHERSCAN_API_KEY
     }
   },
   solcover: {
